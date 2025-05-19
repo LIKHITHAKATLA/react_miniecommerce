11a32b9b21aaaef0cc1628fbd8f81493ffebbafc
LIKHITHAKATLA <likhithakatla1002@gmail.com>
2025-05-19 12:49:28 +0530

    third commit

--- src/Components/Recipes/RecipeDetails.jsx
+++ src/Components/Recipes/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import './RecipeDetails.css';
+import './Recipedetails.css';
 import Spinner from 'react-bootstrap/Spinner';
 
 const RecipeDetails = ({ recipedata }) => {
